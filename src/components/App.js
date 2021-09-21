d753e05d71ba119422a457b75cea8b051d869d50
fidellim <lim.fidel@gmail.com>
2021-09-21 21:51:53 +0400

    add fetch functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,75 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import GithubProfile from "./GithubProfile";
 import Header from "./Header";
 
 const App = () => {
+	const [user, setUser] = useState("");
+	const [userData, setUserData] = useState({});
+	const [userRepos, setUserRepos] = useState([]);
+	const [userCreated, setUserCreated] = useState({});
+	const [isUserExist, setIsUserExist] = useState("empty");
+	const [query, setQuery] = useState(user);
+
+	const api = `https://api.github.com/users/${user}`;
+	const apiRepos = `https://api.github.com/users/${user}/repos?sort=created`;
+
+	const getUserData = async () => {
+		const response = await fetch(api);
+
+		if (response.ok) {
+			setIsUserExist("true");
+			const data = await response.json();
+			setUserData(data);
+
+			let date = await new Date(data.created_at);
+			let month = date.getMonth();
+			let day = date.getDate();
+			let year = date.getFullYear();
+
+			setUserCreated({
+				month: month,
+				day: day,
+				year: year,
+			});
+
+			getUserRepos();
+		} else {
+			setIsUserExist("false");
+		}
+	};
+	const getUserRepos = async () => {
+		const response = await fetch(apiRepos);
+		const data = await response.json();
+		setUserRepos(data);
+	};
+
+	// useEffect(() => {
+
+	// }, [query]);
+
+	const updateSearch = (e) => {
+		setUser(e.target.value);
+	};
+
+	const handleSearch = (e) => {
+		e.preventDefault();
+		setQuery(user);
+		getUserData();
+	};
+
 	return (
 		<div className="App">
 			<Header />
-			<GithubProfile />
+			<GithubProfile
+				data={userData}
+				repos={userRepos}
+				userCreation={userCreated}
+				query={query}
+				user={user}
+				updateSearch={updateSearch}
+				handleSearch={handleSearch}
+				isUserExist={isUserExist}
+			/>
 		</div>
 	);
 };
--- src/components/GithubProfile.js
+++ src/components/GithubProfile.js
@@ -1,53 +1,100 @@
 import React from "react";
 
-const GithubProfile = () => {
+const GithubProfile = ({
+	query,
+	data,
+	repos,
+	userCreation,
+	handleSearch,
+	updateSearch,
+	user,
+	isUserExist,
+}) => {
+	const getMonth = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"June",
+		"July",
+		"Aug",
+		"Sept",
+		"Oct",
+		"Nov",
+		"Dec",
+	];
+
 	return (
 		<main>
 			<div className="inputUserContainer">
 				<form>
-					<i class="fas fa-search"></i>
+					<i className="fas fa-search"></i>
 					<input
 						type="text"
 						className="inputUser"
 						placeholder="Search Github username_"
+						value={user}
+						onChange={updateSearch}
 					/>
-					<button className="search">Search</button>
+					<button className="search" onClick={handleSearch}>
+						Search
+					</button>
 				</form>
 			</div>
-			<div className="githubProfile">
-				<div>
-					<img
-						src="https://source.unsplash.com/random"
-						alt="random"
-						className="random"
-					/>
+			{isUserExist === "true" && query && (
+				<div className="githubProfile">
 					<div>
-						<h2>Fidel Lim</h2>
-						<h3>fidellim</h3>
-						<h3>Joined 25 Jan 2011</h3>
+						<img
+							src={
+								data.avatar_url
+									? data.avatar_url
+									: "https://source.unsplash.com/random"
+							}
+							alt={data.name ? data.name : data.login}
+							className="githubProfileImage"
+						/>
+						<div>
+							<h2>{data.name ? data.name : data.login}</h2>
+							<h3>
+								Joined{" "}
+								{`${userCreation.day} ${getMonth[userCreation.month]} ${
+									userCreation.year
+								}`}
+							</h3>
+						</div>
+					</div>
+					<div className="stats">
+						<div className="stat">
+							<h4>Repos</h4>
+							<p>{data.public_repos}</p>
+						</div>
+						<div className="stat">
+							<h4>Followers</h4>
+							<p>{data.followers}</p>
+						</div>
+						<div className="stat">
+							<h4>Following</h4>
+							<p>{data.following}</p>
+						</div>
+					</div>
+					<h2>Project/s</h2>
+					<div className="projects">
+						{repos.map((repo) => {
+							return (
+								<div key={repo.name} className="project">
+									{repo.name}
+								</div>
+							);
+						})}
 					</div>
 				</div>
-				<div className="stats">
-					<div className="stat">
-						<h4>Repos</h4>
-						<p>8</p>
-					</div>
-					<div className="stat">
-						<h4>Followers</h4>
-						<p>3938</p>
-					</div>
-					<div className="stat">
-						<h4>Following</h4>
-						<p>9</p>
-					</div>
+			)}
+			{isUserExist === "false" && query && (
+				<div className="githubError">
+					<h1>This Github Profile doesn't exist.</h1>
 				</div>
-				<h2>Project/s</h2>
-				<div className="projects">
-					<div className="project">Project1</div>
-					<div className="project">Project1</div>
-					<div className="project">Project1</div>
-				</div>
-			</div>
+			)}
 		</main>
 	);
 };
