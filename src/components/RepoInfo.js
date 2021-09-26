08028b4504baf77d3c5d9634e11e920c04427ed4
fidellim <lim.fidel@gmail.com>
2021-09-26 22:56:08 +0400

    add different element if contributor/s don't exist

--- src/components/RepoInfo.js
+++ src/components/RepoInfo.js
@@ -1,21 +1,15 @@
 import { React, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Octokit } from "@octokit/core";
 
 const RepoInfo = ({ repoName, user }) => {
 	const [repoData, setRepoData] = useState({});
 	const [contributors, setContributors] = useState([]);
 	const { name, description, html_url } = repoData;
 	const [desc, setDesc] = useState("");
-	const [commits, setCommits] = useState({});
 
 	useEffect(() => {
 		const apiRepo = `https://api.github.com/repos/${user}/${repoName}`;
-		const apiRepoContributors = `https://api.github.com/repos/${user}/${repoName}/contributors`;
-		// use to fetch commits
-		const octokit = new Octokit({
-			auth: "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",
-		});
+		const apiRepoContributors = `https://api.github.com/repos/${user}/${repoName}/stats/contributors`;
 
 		const getRepoData = async () => {
 			const response = await fetch(apiRepo);
@@ -36,36 +30,10 @@ const RepoInfo = ({ repoName, user }) => {
 
 		const getRepoContributors = async () => {
 			const response = await fetch(apiRepoContributors);
-
 			if (response.ok) {
 				const data = await response.json();
 				setContributors(data);
-
-				getCommits();
 			}
-		};
-
-		const getCommits = async () => {
-			const response = await octokit.request(
-				"GET /repos/{owner}/{repo}/commits",
-				{
-					owner: user,
-					repo: repoName,
-					per_page: 100,
-				}
-			);
-
-			// if author name is not a key create
-			// and increment value once it occurs again
-			setCommits(
-				response.data.reduce((acc, currentValue) => {
-					if (!acc[currentValue["author"]["login"]]) {
-						acc[currentValue["author"]["login"]] = 0;
-					}
-					acc[currentValue["author"]["login"]] += 1;
-					return acc;
-				}, {})
-			);
 		};
 
 		getRepoData();
@@ -95,27 +63,31 @@ const RepoInfo = ({ repoName, user }) => {
 						<h1>{name}</h1>
 						{description && <p>{desc}</p>}
 						{!description && <p>No description</p>}
-						<h3>Contributor/s</h3>
+						{contributors && <h3>Contributor/s</h3>}
 						<div className="repoContributors">
-							{contributors.map((contributor) => {
-								return (
-									<a
-										key={contributor.login}
-										href={contributor.html_url}
-										target="_blank"
-										rel="noreferrer"
-									>
-										<button className="repoContributor">
-											<h3>{contributor.login}</h3>
-											<hr />
-											<h3>
-												Commits:
-												<span> {commits[contributor.login]}</span>
-											</h3>
-										</button>
-									</a>
-								);
-							})}
+							{Array.isArray(contributors) &&
+								contributors.map((contributor) => {
+									return (
+										<a
+											key={contributor.author.login}
+											href={contributor.author.html_url}
+											target="_blank"
+											rel="noreferrer"
+										>
+											<button className="repoContributor">
+												<h3>{contributor.author.login}</h3>
+												<hr />
+												<h3>
+													Commits:
+													<span> {contributor.total}</span>
+												</h3>
+											</button>
+										</a>
+									);
+								})}
+							{!Array.isArray(contributors) && (
+								<p>There are no contributor/s</p>
+							)}
 						</div>
 						<button className="repoLink">
 							<a href={html_url} target="_blank" rel="noreferrer">
